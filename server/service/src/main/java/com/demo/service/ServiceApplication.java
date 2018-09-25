900e4db20fedf2dba629ddd4b67627f5edb9a528
clcfree <clcfree@outlook.com>
2018-09-26 00:51:23 +0800

    update

--- server/service/src/main/java/com/demo/service/ServiceApplication.java
+++ server/service/src/main/java/com/demo/service/ServiceApplication.java
@@ -1,12 +1,12 @@
-package com.example.demo;
+package com.demo.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ServiceApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ServiceApplication.class, args);
 	}
 }
