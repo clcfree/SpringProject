3adcbdbdaf66e79f8cd3643c05b1f06ab5330e71
clcfree <clcfree@outlook.com>
2018-10-13 22:48:31 +0800

    system

---
+++ server/system/src/test/java/com/demo/server/system/SystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.server.system;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
