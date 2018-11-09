a2271e3df3af1c88cae17811deeb0e0ef842bf34
root <changhong>
2018-11-09 17:35:14 +0800

    update

---
+++ server/monitor-admin/src/main/java/com/demo/server/admin/MonitorAdminApplication.java
@@ -0,0 +1,13 @@
+package com.demo.server.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class MonitorAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorAdminApplication.class, args);
+    }
+}
